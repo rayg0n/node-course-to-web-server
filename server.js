81232f505a6962238a9287e570a50f8d8307f6e9
Raygon <rockyracy@gmail.com>
2018-09-25 19:13:04 +0545

    setup start scrip and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require ('express');
 const fs = require('fs');
 const hbs= require('hbs');
+const port=process.env.PORT || 3000 ;
 var app= express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', hbs);
@@ -56,6 +57,6 @@ app.get('/bad',(req,res)=>{
         error: '404 not found' 
     });
 });
-app.listen(3000,()=>{
-    console.log('server is up on port 3000');
+app.listen(port,()=>{
+    console.log(`server is up on port ${port}`);
 });
